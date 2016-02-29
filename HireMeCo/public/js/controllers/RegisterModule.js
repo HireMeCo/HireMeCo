16266ca967e5dac6890e438032e49c32ce7285fb
cson25 <c.son25@gmail.com>
2016-02-28 18:01:57 -0600

    sortable list update

--- HireMeCo/public/js/controllers/RegisterModule.js
+++ HireMeCo/public/js/controllers/RegisterModule.js
@@ -3,9 +3,10 @@
 RegisterModule.controller('RegisterCtrl',
   ['$scope', '$location', 'AuthService',
     function ($scope, $location, AuthService) {
-        
+        'use strict';
         console.log(AuthService.getUserStatus());
         
+        // =========== REGISTER ====================
         $scope.register = function () {
             
             // initial values
@@ -18,7 +19,7 @@ RegisterModule.controller('RegisterCtrl',
                 $scope.registerForm.password, 
                 $scope.registerForm.firstname, 
                 $scope.registerForm.accountType
-                )
+            )
 
         // handle success
         .then(function () {
@@ -37,4 +38,61 @@ RegisterModule.controller('RegisterCtrl',
 
         };
 
+        //================== SORTABLE LIST ==============================
+        
+
+        
+        $scope.list = [
+            { text: "hello", value: "world" },
+            { text: "hello", value: "world" },
+            { text: "hello", value: "world" },
+            { text: "hello", value: "world" },
+            { text: "hello", value: "world" }
+        ];//myapp.buildArray('Item', 5);
+        
+        $scope.sortingLog = [];
+        
+        $scope.sortableOptions = {
+            // called after a node is dropped
+            stop: function (e, ui) {
+                var logEntry = {
+                    ID: $scope.sortingLog.length + 1,
+                    Text: 'Moved element: ' + ui.item.scope().item.text
+                };
+                $scope.sortingLog.push(logEntry);
+            }
+        };
+        
+
+        $scope.sortSkills = function () {
+            var sortableEle;
+
+            $scope.sortableArray = [
+                'One', 'Two', 'Three'
+            ];
+            
+            $scope.add = function () {
+                $scope.sortableArray.push('Item: ' + $scope.sortableArray.length);
+                
+                sortableEle.refresh();
+            }
+            
+            $scope.dragStart = function (e, ui) {
+                ui.item.data('start', ui.item.index());
+            }
+            $scope.dragEnd = function (e, ui) {
+                var start = ui.item.data('start'),
+                    end = ui.item.index();
+                
+                $scope.sortableArray.splice(end, 0, 
+            $scope.sortableArray.splice(start, 1)[0]);
+                
+                $scope.$apply();
+            }
+            
+            sortableEle = $('#sortable').sortable({
+                start: $scope.dragStart,
+                update: $scope.dragEndw
+            });
+        }
     }]);
